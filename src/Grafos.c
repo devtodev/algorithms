730def969ef32e3d93f756590524290594d99b27
Carlos Miguens <cmiguens@gmail.com>
2016-11-24 17:43:53 -0300

    Grafos & Roads in Hackerland

--- src/Grafos.c
+++ src/Grafos.c
@@ -3,17 +3,16 @@
  *
  *  Created on: 23 de nov. de 2016
  *      Author: karl
- */
+
 
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
 #include <stdlib.h>
-#include <math.h>
 
 typedef struct sRoad tRoad;
 typedef struct sCity tCity;
-typedef struct sCalculated tCalculated;
+
 
 struct sCity{
 	unsigned long id;
@@ -28,16 +27,11 @@ struct sRoad{
 	 tRoad *next, *previous;
 };
 
-struct sCalculated{
-	 long from, to;
-	 tCalculated *next;
-};
-
 // 	N cities   M roads
 long N, M, minPath = LONG_MAX;
 // read inputs
 tCity *initialCity, *cursor;
-tCalculated *needCalculated;
+
 tCity *getCity(unsigned long id)
 {
 	tCity *temp = initialCity;
@@ -103,12 +97,9 @@ void insertRoad(tCity * cityFrom, tCity *cityDestiny, int length)
 
 void addRoad(tCity * cityFrom, tCity *cityDestiny, int length)
 {
-	long size = 1;
-	for(int i = 1; i <= length; i++)
-		size = size * 2;
 	cursor = initialCity;
-	insertRoad(cityFrom, cityDestiny, size); // bidirectional
-	insertRoad(cityDestiny, cityFrom, size);
+	insertRoad(cityFrom, cityDestiny, length); // bidirectional
+	insertRoad(cityDestiny, cityFrom, length);
 }
 
 long tempLength;
@@ -141,65 +132,21 @@ long visitCity(tCity * cityFrom, tCity *cityDestiny, long pathsize)
 	return 0;
 }
 
-int needVisit(int x, int y)
-{
-	tCalculated *cursor = needCalculated;
-	if (x == y) return 0;
-	while ((cursor != NULL) && (cursor->next != NULL))
-	{
-		if (((cursor->from == x) && (cursor->to == y)) ||
-			((cursor->from == y) && (cursor->to == x)) )
-		{
-			return 0;
-		}
-		cursor = cursor->next;
-	}
-	if (cursor == NULL)
-	{
-		needCalculated = malloc(sizeof(struct sCalculated));
-		cursor = needCalculated;
-	} else {
-		if (((cursor->from == x) && (cursor->to == y)) ||
-			((cursor->from == y) && (cursor->to == x)) )
-		{
-			return 0;
-		}
-		cursor->next = malloc(sizeof(struct sCalculated));
-		cursor = cursor->next;
-	}
-	cursor->from = x;
-	cursor->to = y;
-	cursor->next = NULL;
-
-	return 1;
-}
-
-int main() {
+int grafosMain() {
 	int N = 5; // N cities  M roads
-	needCalculated = NULL;
-	long sum = 0;
-
 	for (int id = 1; id <= N; id++)
 		addCity(id);
 
 	addRoad(getCity(1), getCity(3), 5);
-	addRoad(getCity(4), getCity(5), 0);
+	addRoad(getCity(4), getCity(5), 1);
 	addRoad(getCity(2), getCity(1), 3);
 	addRoad(getCity(3), getCity(2), 1);
 	addRoad(getCity(4), getCity(3), 4);
 	addRoad(getCity(4), getCity(2), 2);
 
-	for (int y = 1; y <= N; y++ )
-		for (int x = 1; x <= N; x++ )
-		{
-			if (needVisit(x, y) == 1)
-			{
-				minPath = LONG_MAX;
-				visitCity(getCity(y), getCity(x), 0);
-				sum = sum + minPath;
-			}
-		}
-	printf("%ld ", sum);
-	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	visitCity(getCity(1), getCity(5), 0);
+	printf("%ld", minPath);
+
     return 0;
 }
+ */
