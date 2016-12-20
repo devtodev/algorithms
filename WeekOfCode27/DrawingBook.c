c108bd0d8d475779206ae5f923d82490f63ba4e5
CarlosMiguens <cmiguens@gmail.com>
2016-12-20 00:08:40 -0300

    WOC27-DrawingBook

---
+++ WeekOfCode27/DrawingBook.c
@@ -0,0 +1,27 @@
+/*
+ * DrawingBook.c
+ *
+ *  Created on: 19 de dic. de 2016
+ *      Author: karl
+ */
+
+
+#include "stdio.h"
+
+int main()
+{
+	int n, p, rst;
+	scanf("%d %d", &n, &p);
+	if ((n/2) >= p)
+	{
+		rst = p / 2;
+	}
+	else
+	{
+		rst = (n - p) / 2;
+		if (rst == 0)
+			rst = ((n != p) && (n % 2 == 0))?1:0;
+	}
+	printf("%d", rst);
+}
+
